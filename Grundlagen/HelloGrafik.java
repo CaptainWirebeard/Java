a331c7da03eafe67c159156902bad8ecf322573e
CaptainWirebeard <captainwirebeard@gmx.de>
2015-06-25 11:35:05 +0200

    Create HelloGrafik.java

---
+++ Grundlagen/HelloGrafik.java
@@ -0,0 +1,26 @@
+import javax.swing.JLabel
+import javax.swing.JFrame
+
+public class HelloGrafik{
+
+  // Deklaration und Initialisierung von 
+  // - globalen statischen Konstanten
+  // - globalen statischen Variablen
+  
+  public static void main (String[] args){
+  
+    // Deklaration und Initialisierung von 
+    // - lokalen Konstanten
+    // - lokalen Variablen
+    
+    // erzeuge neues Frame
+    JFrame mainFrame = new JFrame;
+    // Breite: 800pixel, Hoehe: 600pixel
+    mainFrame.setSize(800, 600);
+    // Label hinzufuegen
+    mainFrame.add(new JLabel("Hello Grafik!"));
+    // Frame anzeigen lassen
+    mainFrame.setVisible(true);
+
+  }
+}
